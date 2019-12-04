3d021cda2d5ff4d283b6c840eede37f931bba413
Regina Hansson <regha434@student.liu.se>
2019-12-04 07:02:43 +0100

    Day 4

---
+++ day4.py
@@ -0,0 +1,39 @@
+data = open("input/day4.txt")
+data = data.read()
+
+low = int(data[:6])
+high = int(data[7:13])
+
+print("A: ")
+
+passwordsA = 0
+passwordsB = 0
+for candidate in range(low, high + 1):
+    candidate = str(candidate)
+    decreasing = A = B = False
+    doubles = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0}
+
+    for i in range(5):
+        left = candidate[i]
+        right = candidate[i + 1]
+        if left > right:
+            decreasing = True
+        if left == right:
+            doubles[int(left)] += 1 
+
+    if not decreasing:
+        for n in range(10):
+            if doubles[n] > 0:
+                A = True
+            if doubles[n] == 1:
+                B = True
+        if A:
+            passwordsA += 1
+        if B:
+            passwordsB += 1
+
+print(passwordsA)
+
+print("B: ")
+
+print(passwordsB)
