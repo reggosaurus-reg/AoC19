bb43a73aee48b92e0ea01afe298f228591d2639f
Regina Hansson <regha434@student.liu.se>
2019-12-05 13:21:39 +0100

    Day 5 cleanup (since revisiting old code is apparently a thing)

--- day5.py
+++ day5.py
@@ -2,51 +2,47 @@ data = open("input/day5.txt")
 seqA = list(map(int, data.readline().split(',')))
 seqB = seqA.copy()
 
+get_opcode = lambda code: code % 100
+get_mode = lambda code, i: (code % (10**(i + 2))) // (10**(i + 1)) 
+
 print("A: ")
 
 def simulate_program(seq):
     i = 0
     while i < len(seq):
         code = seq[i]
-        opcode = code % 100
-        mode1 = (code % 1000) // 100 
-        mode2 = (code % 10000) // 1000 
-        mode3 = (code % 100000) // 10000 
+
+        def get_param(pos):
+            """ Returns the value for the pos:th parameter. """
+            mode = get_mode(code, pos)
+            if mode == 0:
+                return seq[seq[i + pos]]
+            else:
+                return seq[i + pos]
+
+        opcode = get_opcode(code)
         if opcode == 99:
             break
         if opcode == 1:
-            if mode1 == 0:
-                op1 = seq[seq[i + 1]]
-            else:
-                op1 = seq[i + 1] 
-            if mode2 == 0:
-                op2 = seq[seq[i + 2]]
-            else: 
-                op2 = seq[i + 2]
+            op1 = get_param(1)
+            op2 = get_param(2)
             res = seq[i + 3]
             seq[res] = op1 + op2  
             i += 4
         if opcode == 2:
-            if mode1 == 0:
-                op1 = seq[seq[i + 1]]
-            else:
-                op1 = seq[i + 1]
-            if mode2 == 0:
-                op2 = seq[seq[i + 2]]
-            else: 
-                op2 = seq[i + 2]
+            op1 = get_param(1)
+            op2 = get_param(2)
             res = seq[i + 3]
             seq[res] = op1 * op2  
             i += 4
         if opcode == 3:
             res = seq[i + 1]
-            seq[res] = int(input("Enter an integer!: "))
+            seq[res] = int(input("Enter an integer! (1 for default elf TEST): "))
             i += 2
         if opcode == 4:
             res = seq[i + 1]
             print(seq[res])
             i += 2
-
 
 simulate_program(seqA)
 
@@ -55,73 +51,70 @@ print("B: ")
 def simulate_warmer_program(seq):
     i = 0
     while i < len(seq):
-        def value(mode, off):
-            if mode == 0:
-                return seq[seq[i + off]]
-            else:
-                return seq[i + off]
         code = seq[i]
-        opcode = code % 100
-        mode1 = (code % 1000) // 100 
-        mode2 = (code % 10000) // 1000 
-        mode3 = (code % 100000) // 10000 
-        if opcode > 10:
+
+        def get_param(pos):
+            """ Returns the value for the pos:th parameter. """
+            mode = get_mode(code, pos)
+            if mode == 0:
+                return seq[seq[i + pos]]
+            else:
+                return seq[i + pos]
+
+        opcode = get_opcode(code)
+        if opcode == 99: # Terminate
             break
-        if opcode == 99:
-            break
-        if opcode == 1:
-            op1 = value(mode1, 1)
-            op2 = value(mode2, 2)
+        if opcode == 1: # Add
+            op1 = get_param(1)
+            op2 = get_param(2)
             res = seq[i + 3]
             seq[res] = op1 + op2  
             i += 4
-        if opcode == 2:
-            op1 = value(mode1, 1)
-            op2 = value(mode2, 2)
+        if opcode == 2: # Multiply
+            op1 = get_param(1)
+            op2 = get_param(2)
             res = seq[i + 3]
             seq[res] = op1 * op2  
             i += 4
-        if opcode == 3:
+        if opcode == 3: # Input
             res = seq[i + 1]
-            seq[res] = int(input("Enter an integer!: "))
+            seq[res] = int(input("Enter an integer! (5 for default elf TEST): "))
             i += 2
-        if opcode == 4:
+        if opcode == 4: # Output
             res = seq[i + 1]
             print(seq[res])
             i += 2
-        if opcode == 5:
-            op1 = value(mode1, 1)
-            op2 = value(mode2, 2)
+        if opcode == 5: # Jump if true
+            op1 = get_param(1)
+            op2 = get_param(2)
             if op1 != 0:
                 i = op2
             else:
                 i += 3
-        if opcode == 6:
-            op1 = value(mode1, 1)
-            op2 = value(mode2, 2)
+        if opcode == 6: # Jump if false
+            op1 = get_param(1)
+            op2 = get_param(2)
             if op1 == 0:
                 i = op2
             else:
                 i += 3
-        if opcode == 7:
-            op1 = value(mode1, 1)
-            op2 = value(mode2, 2)
+        if opcode == 7: # Less than
+            op1 = get_param(1)
+            op2 = get_param(2)
             res = seq[i + 3]
             if op1 < op2:
                 seq[res] = 1
             else:
                 seq[res] = 0
             i += 4
-        if opcode == 8:
-            op1 = value(mode1, 1)
-            op2 = value(mode2, 2)
-            op3 = value(mode3, 3)
+        if opcode == 8: # Equals
+            op1 = get_param(1)
+            op2 = get_param(2)
             res = seq[i + 3]
             if op1 == op2:
                 seq[res] = 1
             else:
                 seq[res] = 0
             i += 4
-
 
 simulate_warmer_program(seqB)
