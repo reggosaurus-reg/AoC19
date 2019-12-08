d8d290d2175b08c9810eb05f3eaf1893fcdae8bf
Regina Hansson <regha434@student.liu.se>
2019-12-08 12:15:29 +0100

    Day 8 improvements

--- day8.py
+++ day8.py
@@ -1,3 +1,4 @@
+from functools import reduce
 digits = open("input/day8.txt").readline()
 
 print("A:")
@@ -5,39 +6,25 @@ print("A:")
 width = 25   
 height = 6
 layer_size = width * height
-layers = []
+layers = [digits[left: left + layer_size] 
+            for left in range(0, len(digits) - 1, layer_size)]
 
-# Divide digits into (flat) layers
-while (len(digits) >= layer_size):
-    layers.append(digits[:layer_size])
-    digits = digits[layer_size:]
-
-# Find layer with least 0's
-minLayer = -1
-currentMin = float('inf')
-for layer in layers:
-    num = layer.count('0')
-    if num < currentMin:
-        currentMin = num
-        minLayer = layer 
+least_zeros = lambda a, b: a if a.count('0') < b.count('0') else b
+minLayer = reduce(least_zeros, layers) 
 
 print(minLayer.count('1') * minLayer.count('2'))
 
-
 print("B:")
 
-# Go through layers and find visible pixels
+# Go through layers and print visible pixels
+color = lambda x: 'X' if x == '1' else ' '
 new_layer = ""
-for pixel_pos in range(layer_size):
-    layer_level = 0
-    pixel = '2' 
-    while (pixel == '2'):
-        pixel = layers[layer_level][pixel_pos]
-        layer_level += 1
-    new_layer += pixel
-
-
-new_layer = new_layer.replace("1", 'X')
-new_layer = new_layer.replace("0", ' ')
-for i in range(height):
-    print(new_layer[i * width: (i + 1) * width])
+for h in range(height):
+    for w in range(width):
+        layer_level = 0
+        pixel = '2' 
+        while (pixel == '2'): # layer invisible - look at next
+            pixel = layers[layer_level][h * width + w]
+            layer_level += 1
+        print(color(pixel), end='')
+    print()
