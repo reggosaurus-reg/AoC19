386b24d6c6416c075cebd3fc2456579de7fc6749
Regina Hansson <regha434@student.liu.se>
2019-12-14 16:58:40 +0100

    Finally, day 10b angles fixed.. and got a nice visualization!

--- day10b.py
+++ day10b.py
@@ -1,63 +1,77 @@
-#data = open("input/day10.txt")
-data = open("test.txt")
+from math import acos, atan, atan2, pi
+import os
+data = open("input/day10.txt")
+#data = open("test10.txt")
 asteroids = []
 for row in data:
-    asteroids.append(row[:-1])
+    asteroids.append(list(row[:-1]))
 
-print("A:")
+def print_asteroids():
+    for row in asteroids:
+        for a in row:
+            print(a, end='')
+        print()
 
-def slopeTo(origo, b):
-    # NEver used here
-    if origo[0] == b[0] and origo[1] == b[1]:
-        return 'same'
+print("B:")
 
-    if origo[1] == b[1]:
-        slope = '0'
-    elif origo[0] == b[0]:
-        slope = '0'
-    else:
-        slope = str( (b[1] - origo[1]) / (b[0] - origo[0]) )
 
-    if b[0] < origo[0] and b[1] >= origo[1]:
-        return 'a' + slope 
-    if b[0] >= origo[0] and b[1] > origo[1]:
-        return 'b' + slope 
-    if b[0] > origo[0] and b[1] <= origo[1]:
-        return 'c' + slope 
-    if b[0] <= origo[0] and b[1] < origo[1]:
-        return 'd' + slope 
+dist = lambda b: abs(b[0] - bestStation[0]) + abs(b[1] - bestStation[1]) 
+angle = lambda b: (atan2(bestStation[1] - b[1], bestStation[0] - b[0])) % (2*pi)
+char = lambda t: asteroids[t[0]][t[1]]
+def set_char(t, c): asteroids[t[0]][t[1]] = c
 
-stations = {} 
-vaporized = 0
+width, height = len(asteroids[0]), len(asteroids) 
+best = (3, 8)
+best = (13, 11)
+best = (19, 20) #from A?
+bestStation = best
+stations = {}
 for y in range(len(asteroids)):
     row = asteroids[y]
     for x in range(len(row)):
         station = asteroids[y][x]
-        if station != '.':
-            stations[(x, y)] = 0
-            
-for origo in stations:
-    #slopes = set() 
-    slopes = {}
-    #slopes = []
-    for asteroid in stations:
-        if asteroid == origo:
+        if station != '.' and (y, x) != bestStation:
+            stations[(y, x)] = 0
+
+
+vaporized = 0
+print_asteroids()
+slopes = {}
+for asteroid in stations:
+    slope = angle(asteroid)
+    if slope in slopes:
+        slopes[slope].append(asteroid)
+    else:
+        slopes[slope] = [asteroid]
+for slope in slopes:
+    pos = slopes[slope]
+    slopes[slope] = sorted(pos, key=dist)
+
+slopeList = list(reversed(sorted(slopes)))
+if slopeList[-1] == 0.0:
+    slopeList = [0.0] + slopeList[:-1]
+
+while slopeList:
+    for slope in slopeList:
+        if not slopes[slope]:
             continue
-        slope = slopeTo(origo, asteroid)
-        if slope not in slopes:
-            slopes[slope] = asteroid # TODO: Invert dict?
-        #slopes.add(slope)
-        #if slope not in slopes:
-        #    slopes.append(slope)
-    #slopes.sort()
-    stations[origo] = slopes
+        coord = slopes[slope][0]
+        if vaporized == 199:
+            print(coord)
+            print(slope)
+            slopeList = 0
+            break
+        new_list = slopes[slope][1:]  
+        set_char(coord, ",")
+        vaporized += 1
+
+        #input("Step? ")
+        os.system("clear")
+        print("B:")
+        print_asteroids()
+
+        slopes[slope] = new_list
 
 
-bestStation = max(stations, key = lambda x: len(stations[x]))
-slopes = stations[bestStation]
-print(len(slopes))
 
-print("B:")
-print(bestStation)
-# TODO: Verify getting corect slopes/ quantiles
-#for entry in stations[origo]:
+# 908, 1025 too high, 525, 316 fel
